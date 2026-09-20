aa216c4cdd8263a39276afbc83e62072d4d702a0
kzw <2592401352@qq.com>
2026-09-20 19:01:07 +0800

    正常跟新

--- calib/calib_camera/calib_camera.py
+++ calib/calib_camera/calib_camera.py
@@ -5,15 +5,54 @@ import sys
 import json
 import logging
 from typing import Dict, List, Tuple
+import logging
 
 import pyapriltags
 import cv2
 import numpy as np
 
+logger = logging.getLogger(__name__)
 
 def calib_camera(
         trags_3d_list:List[pyapriltags.Tag3D],
-        trag_3d_list_list:List[List[pyapriltags.Tag2D]],
+        trag_2d_list_list:List[List[pyapriltags.Tag2D]],
         image_size:Tuple[int,int]        
 )->Tuple[List[float],List[float]]:
+    
+    pts3d_list=[]
+    pts2d_list=[]
+
+    for tag2d_list in trag_2d_list_list:
+
+        pts2d=[]
+        pts3d=[]
+
+        for tag2d in tag2d_list:
+            tag2d_id=tag2d.id
+            tag3d=next(t for t in trags_3d_list if t.id==tag2d_id)
+            if tag3d is None:
+                logger.warning(
+                    f"tagid={tag2d_id} only detected 2d_id skipping it"
+                )
+                continue
+
+            for i in range(4):
+                pts3d.append(np.array(tag3d))
+                pts2d.append(np.array(tag2d))
+        if len(pts3d)<4:
+            logger.warning("valid 3D-2D point pairs found")
+            continue
+
+        pts3d_list.append(np.array(pts3d))
+        pts2d_list.append(np.array(pts2d))
+
+        if len(pts3d_list)<10:
+            logger.warning("valid images with sufficient 3D-2D point pairs found")
+
+            return None,None
+        
+
+
+        
+
     
